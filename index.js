6dfc3f80506359345d776d685439a05d0e51fbfa
mdsafiulazam <mdriadkhan20@gmail.com>
2022-07-09 19:50:28 +0600

    simple count increment and decrement is done

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
+const {createStore}  = require('redux');
+
 const INCREMENT = "INCREMENT"
 const DECREMENT = "DECREMENT"
 
 // state
-const initialCounterState ={
-    count:0,
+const initialCounterState = {
+    count: 0,
 }
-const initialUserState = [
-    {name:'safiul'}
-]
+
 // action - object- type 
 const incrementCount = () => {
+    return {
+        type: INCREMENT,
+    }
+}
+const decrementCount = () => {
+    return {
+        type: DECREMENT,
+    }
+}
 
-    return {
-        type:INCREMENT,
+
+//Reducer pure function
+const counterReducer = (state = initialCounterState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1
+            };
+        case DECREMENT:
+            return {
+                ...state,
+             count: state.count - 1
+            }
+        default:
+            return state
+
     }
 }
-const decrementCount = ()=>{
-    return {
-        type:DECREMENT,
-    }
-}
+
+//store getState(), dispatch(), subscribe()
+//create store
+const store = createStore(counterReducer)
+// subscribe store
+store.subscribe(()=>{
+    console.log(store.getState());
+})
+// dispatch store
+store.dispatch(incrementCount())
+store.dispatch(incrementCount())
+store.dispatch(incrementCount())
+store.dispatch(decrementCount())
+store.dispatch(incrementCount())
